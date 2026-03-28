28142a548276104da59ba814144e2430bef75b90
mal192 <mal_192@mail.ru>
2026-03-29 01:00:51 +0500

    lv38.1

--- src/pointer_S_leval38.c
+++ src/pointer_S_leval38.c
@@ -3,7 +3,71 @@
 //#define sizeStr 10
 
 int main(void){
-	char str[] = "\"Hell0 My Little Friend\"";
-	printf("%s\n", str);
+
+	char str1[10] = "String";
+	char str2[12] = "String";
+	
+	char *pointStr1 = str1;
+	char *pointStr2 = str2;
+
+	int i = 0;
+	for (; pointStr1[i]!='\0' && pointStr2[i]!='\0'; ++i ){
+		if (pointStr1[i] != pointStr2[i])
+		{
+			printf("String simbol not =\n");
+			return 0;
+		}
+		}
+		
+		if (pointStr1[i] != pointStr2[i])
+		{
+			printf("String length not =\n");
+			return 0;
+		}
+		printf("Str1 == Str2\n");
+
+
+// comparison 	string
+
+	char strr1[10] = "StRingS";
+	char strr2[12] = "StRingSQ";
+	
+ int res1;
+ res1 = strcmp(strr1, strr2);
+ 
+ int res2;
+ res2 = strncmp(strr1, strr2, 7);
+ 
+ printf("strr1 == strr2 %d %d\n", res1, res2);
+ //0 - ==
+ // -1 strr1 < strr2
+ // 1 strr1 > strt2
+ 
+ ////find "Fa*" string mass
+ 
+ const char *strS[] = {"Fam", "fabrica", "Fatal", "Ruk", "Fabuca"};
+ char findStr[10] = "Fa";
+ char *pointFindStr = findStr;
+ int countStrS = sizeof(strS)/sizeof(*strS);
+printf("size strS = %d\n", countStrS);
+for (int cycleI = 0; cycleI < countStrS; cycleI++)
+{	
+	if (strncmp(strS[cycleI], pointFindStr, 2) == 0)
+	{
+		printf("%s\n", strS[cycleI]);
+	}
+	
+}
+/// Find simbol
+char findChar = '0';
+char strNewFind[60] = "oOoOo0oOoOooO0oOoOOo0"; //length 5; right 7
+printf("ansii %c = %d\n", findChar, (int)findChar);
+char *pointstrNewFind = strNewFind;
+size_t resultStrchr =  strchr(pointstrNewFind, findChar) - pointstrNewFind;
+printf("index length %c  = %lld\n",findChar, resultStrchr);
+size_t resultStrRchr = strrchr(pointstrNewFind, findChar) - pointstrNewFind;
+printf("index right %c  = %lld\n",findChar, resultStrRchr);
+//time 9:48
+
 	return 0;
 }
