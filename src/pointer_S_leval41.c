f45970054e5a294ce5b50d399810d67cfdb56aa6
mal192 <mal_192@mail.ru>
2026-04-04 23:01:29 +0500

    lv41

--- src/pointer_S_leval41.c
+++ src/pointer_S_leval41.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
-//#define sizeStr 10
+#include <math.h>
 
-float V(float r, float h)
+float sq4(float x)
 {
-return 3.1415*r*r*h;
+ float res = (x < 0) ?  NAN : pow(x, 0.25);
+ return res;
 }
+
+float sq4v2(float x)
+{
+ if (x < 0) return NAN;
+  return pow(x, 0.25);
+}
+
 
 int main(void){
 
-printf("%0.3f\n", V(2,5));
+printf("%0.3f\n", sq4(2.5));
+printf("%0.3f\n", sq4(-2.5));
+printf("%0.3f\n\n", sq4(0));	
 
-	
+printf("%0.3f\n", sq4v2(2.5));
+printf("%0.3f\n", sq4v2(-2.5));
+printf("%0.3f\n", sq4v2(0));	
 	return 0;
 }
