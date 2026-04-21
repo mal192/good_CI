8261ed0226cee837c676f02bcf360422141a5a4c
mal192 <mal_192@mail.ru>
2026-04-21 16:36:26 +0500

    lv46

--- src/pointer_S_leval46.c
+++ src/pointer_S_leval46.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define SIZEM 5
 
-void printPole(char (*p)[SIZEM])
+void printPole(const char (*p)[SIZEM])
 {
 	for (int y = 0; y < SIZEM; y++)
 	{
@@ -24,14 +24,46 @@ void prReC(int i) //recursive function
 	printf("end: %d\n", i);
 }
 
+void recMas(const char (*p)[SIZEM], char (*pPr)[SIZEM], int y, int x)
+{
+	if (p[y][x] != 0 || pPr[y][x] == 1)
+	{	
+		return;
+	}
+	pPr[y][x] = 1;
+	
+	
+	if (y < (SIZEM-1))
+	{
+		recMas(p, pPr, y + 1, x);
+	}
+	if (y > 0)
+	{
+		recMas(p, pPr, y - 1, x);
+	}
+	
+	if (x < (SIZEM-1))
+	{
+		recMas(p, pPr, y, x + 1);
+	}
+	if (x > 0 ) 
+	{
+		recMas(p, pPr, y, x - 1);
+	}
+	
+}
+
 int main(void){
 	
 	prReC(4);
 	printf("\n");
-	char pole[][SIZEM] = {{1, 1, 1, 1, 1}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 1}, {1, 1, 0, 0, 0}, {1, 1, 0, 1, 1}};
+	const char pole[][SIZEM] = {{1, 1, 1, 1, 1}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 1}, {1, 1, 0, 0, 0}, {1, 1, 0, 1, 1}};
 	char PrintPole[SIZEM][SIZEM] = {0};
 	printPole(PrintPole);
 	printPole(pole);
-	
+	printf("function:\n");
+	recMas(pole, PrintPole,  2, 2);
+	printf("print:\n");
+	printPole(PrintPole);
 	return 0;
 }
